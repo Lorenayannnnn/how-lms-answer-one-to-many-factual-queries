12ac117cc907025b137f7fb4881fbcb2e9655bcd
Lorenayannnnn <lorenayan@qq.com>
2025-02-27 10:44:10 -0800

    update script

--- scripts/run_causal_tracing.sh
+++ scripts/run_causal_tracing.sh
@@ -7,7 +7,7 @@ MODEL_NAME="meta-llama/Meta-Llama-3-8B-Instruct2"  # meta-llama/Meta-Llama-3-8B-
 DATASET_NAME="country_cities"       # country_cities, artist_songs, actor_movies
 CACHE_DIR=""
 
-python src/causal_tracing/main.py \
+CUDA_VISIBLE_DEVICES=0 python src/causal_tracing/main.py \
     --project_root_dir "${PROJECT_ROOT_DIR}" \
     --examine_type "${EXAMINE_TYPE}" \
     --model_name "${MODEL_NAME}" \
--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -4,7 +4,7 @@ export PYTHONPATH=:${PYTHONPATH}
 # Set parameters in configs/run_main.yaml file and run this script
 base_config="run_main_configs.yaml"
 
-srun --job-name=llama_1 --gres=gpu:a6000:1 --pty --time=3-0:00 python src/main.py \
+CUDA_VISIBLE_DEVICES=0 python src/main.py \
       --base_configs=configs/${base_config}
 
 #bash scripts/run_main.sh
