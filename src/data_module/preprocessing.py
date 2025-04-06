bc39c7a6c6d6794ac6dadbe3bf47298320ff5911
Lorenayannnnn <lorenayan@qq.com>
2025-04-06 16:39:04 -0700

    Fix preprocessing key name typos

--- src/data_module/preprocessing.py
+++ src/data_module/preprocessing.py
@@ -3,9 +3,9 @@ import torch
 from transformers import AutoTokenizer
 
 
-def tokenize(entry):
+def tokenize(entry, target_answer_idx):
     # Change this according to your needs
-    unused_columns = ["target_answer_idx", "three_answers_start_end_idx", "relation_start_end_idx"]
+    unused_columns = ["target_answer_idx", "relation_start_end_idx"]
     for col in unused_columns:
         if col in entry.keys():
             entry.pop(col)
@@ -14,14 +14,20 @@ def tokenize(entry):
     entry["attention_mask"] = torch.ones(len(entry["input_ids"]))
     entry["subject_first_token"] = entry.pop("subject_token_list")[0]
     three_answers_token_list = entry.pop("three_answers_token_list")
-    entry["three_answers_first_token_list"] = [three_answers_token_list[i][0] for i in range(3)]
+    entry["three_answers_first_token_list"] = [three_answers_token_list[i][0] for i in range(target_answer_idx)]
+    three_answers_start_end_idx = entry.pop("three_answers_start_end_idx")
 
     # Calculate position from right in case of padding from left
     entry["subject_last_token_position_from_right"] = -(len(entry["input_ids"]) - entry["subject_start_end_idx"][1]) - 1
-    if "prev_answer_1_start_end_idx" in entry.keys():
-        entry["right_to_answer_1_last_token"] = -(len(entry["input_ids"]) - entry["prev_answer_1_start_end_idx"][1]) - 1
-    if "prev_answer_2_start_end_idx" in entry.keys():
-        entry["right_to_answer_2_last_token"] = -(len(entry["input_ids"]) - entry["prev_answer_2_start_end_idx"][1]) - 1
+
+    for tmp_idx in range(1, target_answer_idx):
+        entry[f"right_to_answer_{tmp_idx}_last_token"] = -(len(entry["input_ids"]) - three_answers_start_end_idx[tmp_idx - 1][1]) - 1
+        entry[f"prev_answer_{tmp_idx}_start_end_idx"] = three_answers_start_end_idx[tmp_idx - 1]
+
+    # if "prev_answer_1_start_end_idx" in entry.keys():
+    #     entry["right_to_answer_1_last_token"] = -(len(entry["input_ids"]) - entry["prev_answer_1_start_end_idx"][1]) - 1
+    # if "prev_answer_2_start_end_idx" in entry.keys():
+    #     entry["right_to_answer_2_last_token"] = -(len(entry["input_ids"]) - entry["prev_answer_2_start_end_idx"][1]) - 1
 
     entry["last_token_start_end_idx"] = [len(entry["input_ids"]) - 1, len(entry["input_ids"])]
     entry["last_token_position_from_right"] = -1
